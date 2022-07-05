7aee09496141b438b68562584277348566c32fb6
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2022-07-05 15:05:09 +0300

    Update generating_0_1_vectors

--- 1. recursion_and_backtracking_lab/generating_0_1_vectors.py
+++ 1. recursion_and_backtracking_lab/generating_0_1_vectors.py
@@ -1,10 +1,20 @@
 def gen01(idx, vector):
     if idx >= len(vector):
-        print(*vector, sep='')
+        print("".join([str(x) for x in vector]))
         return
-    for num in range(2):
-        vector[idx] = num
+
+    for number in range(0, 2):
+        vector[idx] = number
         gen01(idx + 1, vector)
+
+
+# def gen01(idx, vector):
+# if idx >= len(vector):
+#     print(*vector, sep='')
+#     return
+# for num in range(2):
+#     vector[idx] = num
+#     gen01(idx + 1, vector)
 
 
 # 3
