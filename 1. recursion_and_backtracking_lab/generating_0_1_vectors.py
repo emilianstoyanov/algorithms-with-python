f03c3231956d57b3a97801a9543c967d990651b3
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2022-07-05 14:29:58 +0300

    create generating 0/1 vectors

---
+++ 1. recursion_and_backtracking_lab/generating_0_1_vectors.py
@@ -0,0 +1,28 @@
+def gen01(idx, vector):
+    if idx >= len(vector):
+        print(*vector, sep='')
+        return
+    for num in range(2):
+        vector[idx] = num
+        gen01(idx + 1, vector)
+
+
+# 3
+n = int(input())
+vector = [0] * n
+
+gen01(0, vector)
+
+"""  
+
+000
+001
+010
+011
+100
+101
+110
+111
+
+
+"""
