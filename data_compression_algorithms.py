64f9d8df6ea1ca2a0acdac15e404743311001c66
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-08-02 10:57:49 +0300

    update data compression algorithms

--- data_compression_algorithms.py
+++ data_compression_algorithms.py
@@ -3,12 +3,33 @@ import lzma
 import gzip
 import bz2
 
-data = b'This is some sample data' * 990000
-
+data = b'This is some sample data' * 999000
+print("------------------------------------")
 print('Original data size:', len(data))
 
+print("----------------lzma compress--------------------")
 start = time.time()
 compessed_data_lzma = lzma.compress(data)
 end = time.time()
 
 print(end - start)
+print(f"lzma compress data: {len(compessed_data_lzma)}")
+
+
+print("---------------gzip compress---------------------")
+start = time.time()
+compessed_data_gzip = gzip.compress(data)
+end = time.time()
+
+print(end - start)
+print(f"gzip compress data: {len(compessed_data_gzip)}")
+
+
+
+print("---------------bz2 compress---------------------")
+start = time.time()
+compessed_data_bz2 = bz2.compress(data)
+end = time.time()
+
+print(end - start)
+print(f"bz2 compress data: {len(compessed_data_bz2)}")
